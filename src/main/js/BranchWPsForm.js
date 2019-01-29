09f6d60cef272eb2366ec576c34c04b2caf81ac2
Philipp Czora <philipp.czora@cloudogu.com>
2019-01-29 16:54:14 +0100

    Fixed comparison & prettified file

--- src/main/js/BranchWPsForm.js
+++ src/main/js/BranchWPsForm.js
@@ -1,17 +1,16 @@
 //@flow
 import React from "react";
-import {translate} from "react-i18next";
-import type {BranchWPs} from "./BranchWP";
-import {Button, Checkbox} from "@scm-manager/ui-components";
+import { translate } from "react-i18next";
+import type { BranchWPs } from "./BranchWP";
+import { Button, Checkbox } from "@scm-manager/ui-components";
 import BranchWPComponent from "./BranchWPComponent";
-
 
 type Props = {
   initialConfiguration: BranchWPs,
   readOnly: boolean,
   onConfigurationChange: (BranchWPs, boolean) => void,
   // context prop
-  t: (string) => string
+  t: string => string
 };
 
 type State = BranchWPs & {};
@@ -19,43 +18,50 @@ type State = BranchWPs & {};
 class BranchWPsForm extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
-    this.state = {...props.initialConfiguration};
-  };
+    this.state = { ...props.initialConfiguration };
+  }
 
   isValid() {
-    const {permissions} = this.state;
+    const { permissions } = this.state;
     let valid = true;
-    permissions.map((branchWP) => {
-      valid = valid && branchWP.name.trim() != "" && branchWP.branch.trim() != "" && branchWP.type.trim() != "";
+    permissions.map(branchWP => {
+      valid =
+        valid &&
+        branchWP.name.trim() !== "" &&
+        branchWP.branch.trim() !== "" &&
+        branchWP.type.trim() !== "";
     });
     return valid;
   }
 
   updateBranchWPs(permissions) {
-    this.setState({permissions}, () => this.props.onConfigurationChange(this.state, this.isValid()));
+    this.setState({ permissions }, () =>
+      this.props.onConfigurationChange(this.state, this.isValid())
+    );
   }
 
-  onDelete = (deletedBranchWP) => {
-    const {permissions} = this.state;
+  onDelete = deletedBranchWP => {
+    const { permissions } = this.state;
     let index = permissions.indexOf(deletedBranchWP);
     permissions.splice(index, 1);
     this.updateBranchWPs(permissions);
   };
 
   onChange = (changedBranchWP, index) => {
-    const {permissions} = this.state;
+    const { permissions } = this.state;
     permissions[index] = changedBranchWP;
     this.updateBranchWPs(permissions);
   };
 
-  onChangeEnabled = (isEnabled) => {
-    this.setState({enabled: isEnabled}
-    , () => this.props.onConfigurationChange(this.state, this.isValid()))
+  onChangeEnabled = isEnabled => {
+    this.setState({ enabled: isEnabled }, () =>
+      this.props.onConfigurationChange(this.state, this.isValid())
+    );
   };
 
   render() {
-    const {permissions, enabled} = this.state;
-    const {t, readOnly} = this.props;
+    const { permissions, enabled } = this.state;
+    const { t, readOnly } = this.props;
     let defaultUserBranchWP = {
       branch: "",
       name: "",
@@ -69,41 +75,48 @@ class BranchWPsForm extends React.Component<Props, State> {
       type: "ALLOW"
     };
 
-    const buttons = (<article className="media">
-      <Button disabled={readOnly}
-              label={t("scm-branchwp-plugin.add-user-permission")}
-              action={() => {
-                permissions.push(defaultUserBranchWP);
-                this.updateBranchWPs(permissions);
-              }
-              }/>
-      <Button disabled={readOnly}
-              label={t("scm-branchwp-plugin.add-group-permission")}
-              action={() => {
-                permissions.push(defaultGroupBranchWP);
-                this.updateBranchWPs(permissions);
-              }
-              }/>
-    </article>);
-
+    const buttons = (
+      <article className="media">
+        <Button
+          disabled={readOnly}
+          label={t("scm-branchwp-plugin.add-user-permission")}
+          action={() => {
+            permissions.push(defaultUserBranchWP);
+            this.updateBranchWPs(permissions);
+          }}
+        />
+        <Button
+          disabled={readOnly}
+          label={t("scm-branchwp-plugin.add-group-permission")}
+          action={() => {
+            permissions.push(defaultGroupBranchWP);
+            this.updateBranchWPs(permissions);
+          }}
+        />
+      </article>
+    );
 
     const form = permissions.map((branchWP, index) => {
-      return <BranchWPComponent
-        branchWP={branchWP}
-        readOnly={readOnly}
-        onDelete={this.onDelete}
-        onChange={(changedBranchWP) => this.onChange(changedBranchWP, index)}
-      />
-    }) ;
-
+      return (
+        <BranchWPComponent
+          branchWP={branchWP}
+          readOnly={readOnly}
+          onDelete={this.onDelete}
+          onChange={changedBranchWP => this.onChange(changedBranchWP, index)}
+        />
+      );
+    });
 
     return (
       <>
-        <Checkbox checked={enabled} onChange={this.onChangeEnabled}
-                  label={t("scm-branchwp-plugin.is-enabled")}
-                  helpText={t("scm-branchwp-plugin.is-enabled-help-text")}/>
-        {enabled ? (form): ("")}
-        {enabled ? (buttons): ("")}
+        <Checkbox
+          checked={enabled}
+          onChange={this.onChangeEnabled}
+          label={t("scm-branchwp-plugin.is-enabled")}
+          helpText={t("scm-branchwp-plugin.is-enabled-help-text")}
+        />
+        {enabled ? form : ""}
+        {enabled ? buttons : ""}
       </>
     );
   }
